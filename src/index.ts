4d992a77cdeaecbef996c8e7622edbba2df3fa6a
Balazs Kovacs <kobalazs@gmail.com>
2023-10-05 17:32:44 +0200

    Add linter

--- src/index.ts
+++ src/index.ts
@@ -6,20 +6,18 @@ export const Storage = {
 } as const;
 type StorageKey = (typeof Storage)[keyof typeof Storage];
 
-export const Persist =
-  (key?: string, storage: StorageKey = Storage.LocalStorage) =>
-  (target: any, memberName: string) => {
-    if (key === undefined) {
-      key = memberName;
-    }
+export const Persist = (key?: string, storage: StorageKey = Storage.LocalStorage) => ((target: any, memberName: string) => {
+  if (key === undefined) {
+    key = memberName;
+  }
 
-    let currentValue: any = target[memberName] ?? JSON.parse(window[storage].getItem(key) ?? 'null');
+  let currentValue: any = target[memberName] ?? JSON.parse(window[storage].getItem(key) ?? 'null');
 
-    Object.defineProperty(target, memberName, {
-      set: (newValue: any) => {
-        window[storage].setItem(key as string, JSON.stringify(newValue));
-        currentValue = newValue;
-      },
-      get: () => currentValue,
-    });
-  };
+  Object.defineProperty(target, memberName, {
+    set: (newValue: any) => {
+      window[storage].setItem(key as string, JSON.stringify(newValue));
+      currentValue = newValue;
+    },
+    get: () => currentValue,
+  });
+});
