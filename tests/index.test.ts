16f7dececa89ba0aa0ea111ba875c2e1a07563ba
Balazs Kovacs <kobalazs@gmail.com>
2023-10-09 13:26:12 +0200

    Getter for missing key should return undefined & setter should delete key if value is undefined

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -5,24 +5,37 @@ import { Persist } from '../src/index';
 // https://github.com/jestjs/jest/issues/6798#issuecomment-440988627
 const localStorageGetItemSpy = jest.spyOn(window.localStorage.__proto__, 'getItem');
 const localStorageSetItemSpy = jest.spyOn(window.localStorage.__proto__, 'setItem');
+const localStorageRemoveItemSpy = jest.spyOn(window.localStorage.__proto__, 'removeItem');
 
 class TestBed {
   @Persist() public foo: unknown;
 }
 
-describe('Persist', () => {
+describe('@Persist()', () => {
+  let testBed: TestBed;
+
+  beforeEach(() => {
+    testBed = new TestBed();
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
 
   it('should load value', () => {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const testBed = new TestBed();
     expect(localStorageGetItemSpy).toHaveBeenCalled();
+    expect(testBed.foo).toBeUndefined();
   });
 
-  it('should persist value', () => {
-    (new TestBed()).foo = 'bar';
+  it('should persist scalar value', () => {
+    testBed.foo = 'bar';
     expect(localStorageSetItemSpy).toHaveBeenCalledWith('foo', '"bar"');
+    expect(testBed.foo).toBe('bar');
+  });
+
+  it('should remove key from storage if value is undefined', () => {
+    testBed.foo = undefined;
+    expect(localStorageRemoveItemSpy).toHaveBeenCalledWith('foo');
+    expect(testBed.foo).toBeUndefined();
   });
 });
